649dd27375f8bb513e9a8a51c192aef961ef30c1
“WineWang0000” <1205294873@qq.com>
2019-05-06 20:37:35 +0800

    loading默认false，需要时添加则实现。

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-      <g-icon v-if="icon" :name="icon"></g-icon>
-      <g-icon class="loading" name="loading"></g-icon>
+      <g-icon v-if="icon && !loading" :name="icon"></g-icon>
+      <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
       <div class="content">
         <slot></slot>
       </div>
@@ -11,6 +11,10 @@
   export default{
     props:{
       icon:{},
+      loading:{
+        type: Boolean ,
+        default: false
+      },
       iconPosition:{
         type: String,
         default:'left',
@@ -29,7 +33,6 @@
     0%{transform: rotate(0deg);}
     100%{transform: rotate(360deg);}
   }
-
   .g-button{
     font-size: var(--button-bg);
     background: var(--button-bg);
