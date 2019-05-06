fb8ad0799f7fdf9a548c9c25972ba245a6790c2d
“WineWang0000” <1205294873@qq.com>
2019-05-06 15:54:20 +0800

    验证icon和iconPosition

---
+++ src/button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+  export default{}
+</script>
+<style lang="scss">
+  .g-button{
+    padding: 0 1em;
+    font-size: var(--button-bg);
+    background: var(--button-bg);
+    border-radius: var(--button-border-radius);
+  }
+    &:hover{
+      background: var(--border-color-hover);
+  }
+    &:active{
+      background: var(--button-active-bg);
+      color: var(--button-active-color);
+  }
+    &:focus{outline: none;}
+  </style>
