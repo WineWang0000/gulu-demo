18a449c42fc2a8e3f6c2112b08943e320e830589
“WineWang0000” <1205294873@qq.com>
2019-05-07 10:42:34 +0800

    检测 g-button-group 子元素是不是 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 <script>
+  export default{
+    mounted(){
+      for(let node of this.$el.children){
+        let name = node.nodeName.toLowerCase()
+        if(name !== 'button'){
+          console.warn(`g-button-group 的子元素应该是 g-button，但你写的是 ${name}`)
+        }
+      }
+    }
+  }
 </script>
 <style lang="scss">
   .g-button-group{
