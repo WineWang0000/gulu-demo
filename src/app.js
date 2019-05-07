6c47fb8ae483e8bb0f75b02ec93d7103dc0c203a
“WineWang0000” <1205294873@qq.com>
2019-05-07 12:46:30 +0800

    Expect 单元测试

--- src/app.js
+++ src/app.js
@@ -13,3 +13,20 @@ new Vue({
     loading3: false
   }
 })
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon: 'setting'
+    }
+  })
+  button.$mount('#text')
+  let useEle = button.$el.querySelector('use')
+  console.log(useEle);
+  expect(useEle.getAttribute('xlink:href')).to.eq('#icon-setting')
+}
+//动态生成按钮
