bd18f57bf1d038f7a91c104d6a6434042fee3d53
“WineWang0000” <1205294873@qq.com>
2019-05-07 15:24:54 +0800

    新增几个chai expect 测试

--- src/app.js
+++ src/app.js
@@ -15,18 +15,52 @@ new Vue({
 })
 
 //单元测试
+//这叫动态生成按钮
 import chai from 'chai'
 const expect = chai.expect
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vueModel = new Constructor({
     propsData:{
       icon: 'setting'
     }
   })
-  button.$mount('#text')
-  let useEle = button.$el.querySelector('use')
+  vueModel.$mount('#text')
+  let useEle = vueModel.$el.querySelector('use')
   console.log(useEle);
   expect(useEle.getAttribute('xlink:href')).to.eq('#icon-setting')
+  vueModel.$el.remove()
+  vueModel.$destroy()
 }
-//动态生成按钮
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon: 'setting',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useEle = button.$el.querySelector('use')
+  expect(useEle.getAttribute('xlink:href')).to.eq('#icon-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon: 'setting',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('0')
+  button.$el.remove()
+  button.$destroy()
+}
+
